02914eb403cd51e2b199397b6daab7cfae5a76c1
Markus Tobler <mail@markustobler.ch>
2018-05-31 22:40:01 +0200

    edit and save notes

--- src/js/controllers/edit.js
+++ src/js/controllers/edit.js
@@ -9,14 +9,7 @@ let noteForm = {};
 const main = document.querySelector('main');
 
 
-// create globally-unique identifier
-const uuidv4 = {
-    id: () => {
-        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-        );
-    }
-};
+
 
 
 // app-state / model
@@ -32,33 +25,6 @@ const editModel = {
         else{
             return 0;
         }}
-    ,
-
-    saveNote: function () {
-        const note = new Note({
-            id: uuidv4.id(),
-            title: noteTitle.value,
-            description: noteDescription.value,
-            importance: editModel.importance(),
-            finishDate: noteFinishDate.value,
-            createdDate: moment(),
-            archived: false
-        });
-
-
-        // get notes object from local storage and parse JSON or set new object
-        let notes = JSON.parse(localStorage.getItem('notes')) || {};
-
-        // add note to notes object with note.id as key
-        notes[note.id] = note;
-
-        // parse notes object back to json and store it in local storage
-        localStorage.setItem('notes', JSON.stringify(notes));
-
-        // redirect to index.html
-        window.location.href = 'index.html';
-
-    }
 };
 
 // Controller
@@ -88,7 +54,20 @@ const editController = {
         // add listener submit button
         noteForm.addEventListener('submit', function (e) {
             e.preventDefault();
-            editModel.saveNote();
+
+            const note = new Note({
+                id: noteForm.dataset.id,
+                title: noteTitle.value,
+                description: noteDescription.value,
+                importance: editModel.importance(),
+                finishDate: noteFinishDate.value,
+                createdDate: noteForm.dataset.createdDate,
+                archived: false
+            });
+
+            noteHelpers.saveNote(note);
+            // redirect to index.html
+            window.location.href = 'index.html';
         });
 
         // add listener cancel button
